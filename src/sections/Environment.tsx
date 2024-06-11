3dce4bf13061571ff542829ac0b6ed49f90e34b3
Thu Vo <vothithu0111@gmail.com>
2024-06-11 07:01:11 +0700

    c11

--- src/sections/Environment.tsx
+++ src/sections/Environment.tsx
@@ -2,11 +2,11 @@ import CustomButton from "@/components/CustomButton";
 import Image from "next/image"
 const Environment = () => {
   return (
-    <div className="grid grid-cols-2 space-x-8 animate-slideInRight" name="environment">
+    <div className="grid grid-cols-2 space-x-8 animate-slideInRight" id="environment">
       <img className="rounded-full " width={1400} height={500} src="/image/env3.png" />
       <img className="rounded-full" width={1400} height={500} src="/image/env1.png"/>
     </div>
   );
 }
 
-export default Environment
+export default Environment 
